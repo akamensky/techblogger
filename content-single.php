cf89c08ecfbd686a6da46e4302c17e558947c0b5
Alexey Kamenskiy <alexey.kamenskiy@macbook.local>
2018-06-29 21:41:06 +0800

    Update

--- content-single.php
+++ content-single.php
@@ -1,5 +1,5 @@
 <div class="content">
     <h1><?php the_title(); ?></h1>
-    <span class="content-meta"><?php the_date(); ?></span>
+    <span class="content-meta">/ <?php the_date(); ?> / <?php printf(_nx('1 comment', '%1$s comments', get_comments_number(), ''), number_format_i18n(get_comments_number())); ?> /</span>
     <?php the_content(); ?>
 </div><!-- /.blog-post -->
--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <div class="content">
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <span class="content-meta"><?php the_date(); ?></span>
+    <span class="content-meta">/ <?php the_date(); ?> / <?php printf(_nx('1 comment', '%1$s comments', get_comments_number(), ''), number_format_i18n(get_comments_number())); ?> /</span>
     <?php the_excerpt(); ?>
 </div><!-- /.blog-post -->
