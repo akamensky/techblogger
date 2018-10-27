84ce83222619fa0679a280c90a38ae1b02ef87d1
Alexey Kamenskiy <akamensky@users.noreply.github.com>
2018-10-27 14:53:23 +0800

    Don't show date on pages

--- page.php
+++ page.php
@@ -7,7 +7,6 @@
                 while (have_posts()) :
                     the_post(); ?>
                     <h1 class="post-title"><?php the_title(); ?></h1>
-                    <span class="post-date"><?php the_date(); ?></span>
                     <?php
                     the_content();
                     ?>
